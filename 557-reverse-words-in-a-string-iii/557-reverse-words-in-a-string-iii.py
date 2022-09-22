e433ec3ee89b324a0888c49f006474bef1a3bcd8
Guan-Lin Huang <39217127+allenGOOD7@users.noreply.github.com>
2022-09-22 20:46:42 +0800

    Time: 255 ms (5.04%), Space: 14.6 MB (46.10%) - LeetHub

--- 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.py
+++ 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.py
@@ -1,35 +1,20 @@
 class Solution:
-    def reverseWords(self, s: str) -> str:
-#         arr = []
-#         for char in s:
-#             arr.append(char)
-        
-#         head = tail = 0
-#         while tail < len(s):
-#             if arr[tail] == " ":
-#                 arr[head:tail] = arr[head:tail][::-1]
-#                 head = tail + 1
-#             tail += 1
-        
-#         arr[head:len(s)] = arr[head:len(s)][::-1]
-        
-#         return "".join(arr)
-            
-#         res = ""
-#         head = tail = 0
-#         while tail < len(s):
-#             if s[tail] == " ":
-#                 res += s[head:tail][::-1]
-#                 res += " "
-#                 head = tail + 1
-#             tail += 1
-        
-#         res += s[head:len(s)][::-1]
-#         return res
-    
+    def reverseWords(self, s: str) -> str:            
+        s += " "
         res = ""
-        s = s.split(" ")
-        for string in s:
-            res += " "
-            res += string[::-1]
-        return res[1:]
+        head = tail = 0
+        while tail < len(s):
+            if s[tail] == " ":
+                res += s[head:tail][::-1]
+                res += " "
+                head = tail + 1
+            tail += 1
+        
+        return res[:-1]
+    
+        # res = ""
+        # s = s.split(" ")
+        # for string in s:
+        #     res += " "
+        #     res += string[::-1]
+        # return res[1:]
