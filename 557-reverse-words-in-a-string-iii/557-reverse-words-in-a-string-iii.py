9eb149d1fcbfb6fe94d28f6ff8879b82aeabf75d
Guan-Lin Huang <39217127+allenGOOD7@users.noreply.github.com>
2022-09-22 20:36:41 +0800

    Time: 133 ms (19.58%), Space: 14.7 MB (46.10%) - LeetHub

--- 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.py
+++ 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.py
@@ -1,17 +1,28 @@
 class Solution:
     def reverseWords(self, s: str) -> str:
-        arr = []
-        for char in s:
-            arr.append(char)
+#         arr = []
+#         for char in s:
+#             arr.append(char)
         
+#         head = tail = 0
+#         while tail < len(s):
+#             if arr[tail] == " ":
+#                 arr[head:tail] = arr[head:tail][::-1]
+#                 head = tail + 1
+#             tail += 1
+        
+#         arr[head:len(s)] = arr[head:len(s)][::-1]
+        
+#         return "".join(arr)
+            
+        res = ""
         head = tail = 0
         while tail < len(s):
-            if arr[tail] == " ":
-                arr[head:tail] = arr[head:tail][::-1]
+            if s[tail] == " ":
+                res += s[head:tail][::-1]
+                res += " "
                 head = tail + 1
             tail += 1
         
-        arr[head:len(s)] = arr[head:len(s)][::-1]
-        
-        return "".join(arr)
-            
+        res += s[head:len(s)][::-1]
+        return res
